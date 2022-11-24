e5529b78e7034d3b9b4b64568fa6692decd350f7
ava_kyoko <ava8kyoko@gmail.com>
2022-11-23 21:31:57 -0500

    error message fixed

--- includes/libft/exit_error.c
+++ includes/libft/exit_error.c
@@ -6,7 +6,7 @@
 /*   By: mchampag <mchampag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 12:18:31 by mchampag          #+#    #+#             */
-/*   Updated: 2022/11/12 10:48:57 by mchampag         ###   ########.fr       */
+/*   Updated: 2022/11/23 21:29:05 by mchampag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	exit_error(char *message, char **tab)
 {
 	if (tab)
 		free_tab(tab);
+	ft_putstr_fd(2, "Error", 'n');
 	ft_putstr_fd(2, message, 'n');
 	exit(EXIT_FAILURE);
 }
