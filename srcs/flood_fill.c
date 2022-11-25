e4727d09aca14f1c5064d3a53326f657c08eb90e
ava_kyoko <acid.burn@imacdeavakyoko.home>
2022-11-24 22:43:11 -0500

    flood fill need to be tested

--- srcs/flood_fill.c
+++ srcs/flood_fill.c
@@ -6,7 +6,7 @@
 /*   By: acid.burn <acid.burn@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 22:24:50 by mchampag          #+#    #+#             */
-/*   Updated: 2022/11/24 11:37:32 by acid.burn        ###   ########.fr       */
+/*   Updated: 2022/11/24 22:40:16 by acid.burn        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,15 @@
 
 static void flood_fill(t_data *data, size_t x, size_t y)
 {
-	if (data->floodfill_map[y][x] == '1' || data->floodfill_map[y][x] == 'F')
-		return ;
-	if (data->floodfill_map[y][x] == '0' || data->floodfill_map[y][x] == 'C' ||
-		data->floodfill_map[y][x] == 'E' || data->floodfill_map[y][x] == 'P')
+	if (data->floodfill_map[y][x] == '1' || data->floodfill_map[y][x] == 'E'
+		|| data->floodfill_map[y][x] == 'F')
+			return ;
+	if (data->floodfill_map[y][x] == '0' || data->floodfill_map[y][x] == 'C'
+		|| data->floodfill_map[y][x] == 'P')
 	{
-		if (data->floodfill_map[y][x] == 'E')
-			data->floodfill_exit--;
 		if (data->floodfill_map[y][x] == 'C')
 			data->floodfill_item--;
 		data->floodfill_map[y][x] = 'F';
-		if (!data->floodfill_exit && data->floodfill_item) // need to test
-			return ;
 		flood_fill(data, x, y - 1);
 		flood_fill(data, x, y + 1);
 		flood_fill(data, x - 1, y);
@@ -37,11 +34,11 @@ static void flood_fill(t_data *data, size_t x, size_t y)
 void	valid_floodfill(t_data *data)
 {
 	data->floodfill_map = remalloc_it(data->map, 0, 0);
-	data->floodfill_exit = data->nb_exit;
+	data->floodfill_exit = 0;
 	data->floodfill_item = data->nb_item;
 	flood_fill(data, data->pos_x, data->pos_y);
 	free_tab(data->floodfill_map);
-	if (data->floodfill_exit || data->floodfill_item)
+	if (data->floodfill_item)
 		exit_error("Flood fill failed\n", data->map);	
 	printf("floodfill ok\n");
 }
